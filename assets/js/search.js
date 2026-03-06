71b98af1c1eae9c63d7be7acca3ee0461b27364d
GuillaumeDua <pro.guillaume.dua@gmail.com>
2026-03-07 00:29:37 +0100

    [blog] #5 - search bar in side panel (debug)

--- assets/js/search.js
+++ assets/js/search.js
@@ -32,65 +32,53 @@ function wireSearch(input, resultsEl, docs, idx) {
 
 // Load search index and wire up Lunr
 document.addEventListener('DOMContentLoaded', function() {
-  // Inject sidebar search bar
-  const nav = document.querySelector('.sidebar-nav');
-  if (nav && !document.getElementById('sidebar-search')) {
-    const div = document.createElement('div');
-    div.className = 'sidebar-search';
-    div.innerHTML = `
-      <input type="text" id="sidebar-search-input" placeholder="Search...">
-      <ul id="sidebar-search-results"></ul>
-    `;
-    nav.parentNode.insertBefore(div, nav);
-  }
 
-  fetch('/publications/search.json')
-    .then(r => r.json())
-    .then(data => {
-      const idx = lunr(function() {
-        this.field('title', { boost: 10 });
-        this.field('content');
-        this.field('tags', { boost: 5 });
-        this.ref('url');
-        data.forEach(doc => this.add(doc));
+    console.log('DOMContentLoaded fired');
+
+    // Inject sidebar search bar
+    const nav = document.querySelector('.sidebar-nav');
+    if (nav && !document.getElementById('sidebar-search')) {
+      const div = document.createElement('div');
+      div.className = 'sidebar-search';
+      div.innerHTML = `
+        <input type="text" id="sidebar-search-input" placeholder="Search...">
+        <ul id="sidebar-search-results"></ul>
+      `;
+      nav.parentNode.insertBefore(div, nav);
+    }
+
+    fetch('/publications/search.json')
+      .then(r => r.json())
+      .then(data => {
+          const idx = lunr(function() {
+              this.field('title', { boost: 10 });
+              this.field('content');
+              this.field('tags', { boost: 5 });
+              this.ref('url');
+              data.forEach(doc => this.add(doc));
+          });
+
+          const docs = {};
+          data.forEach(doc => docs[doc.url] = doc);
+
+          // Wire sidebar search
+          const sidebarInput = document.getElementById('sidebar-search-input');
+          const sidebarResults = document.getElementById('sidebar-search-results');
+          console.log('sidebarInput:', pageInput);
+          console.log('sidebarResults:', pageResults);
+          if (sidebarInput && sidebarResults) {
+              console.log('wiring search side-bar');
+              wireSearch(sidebarInput, sidebarResults, docs, idx);
+          }
+
+          // Wire search page
+          const pageInput = document.getElementById('search-input');
+          const pageResults = document.getElementById('search-results');
+          console.log('pageInput:', pageInput);
+          console.log('pageResults:', pageResults);
+          if (pageInput && pageResults) {
+              console.log('wiring search page');
+              wireSearch(pageInput, pageResults, docs, idx);
+          }
       });
-
-      const docs = {};
-      data.forEach(doc => docs[doc.url] = doc);
-
-      // Wire sidebar search
-      const sidebarInput = document.getElementById('sidebar-search-input');
-      const sidebarResults = document.getElementById('sidebar-search-results');
-      if (sidebarInput && sidebarResults) {
-        wireSearch(sidebarInput, sidebarResults, docs, idx);
-      }
-
-      // Wire search page
-      const pageInput = document.getElementById('search-input');
-      const pageResults = document.getElementById('search-results');
-      if (pageInput && pageResults) {
-        wireSearch(pageInput, pageResults, docs, idx);
-      }
-    });
-});
-
-// debug
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('DOMContentLoaded fired');
-  
-  fetch('/publications/search.json')
-    .then(r => r.json())
-    .then(data => {
-      console.log('fetch done');
-      
-      const pageInput = document.getElementById('search-input');
-      const pageResults = document.getElementById('search-results');
-      console.log('pageInput:', pageInput);
-      console.log('pageResults:', pageResults);
-
-      if (pageInput && pageResults) {
-        console.log('wiring search page');
-        wireSearch(pageInput, pageResults, docs, idx);
-      }
-    });
 });
